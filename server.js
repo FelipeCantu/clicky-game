96e3d5094254cb6099a6c85d532542e95bf9e85c
Felipe Cantu <36748934+ABACABB00@users.noreply.github.com>
2020-06-22 20:54:42 -0500

    PROD

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use("/api", api);
 
 if(process.env.PROD) {
     app.use(express.static(path.join(_dirname, './client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, './client/build/index.html'));
+    })
 }
 
 const port = process.env.PORT || 5000;
