7c75ae319d80515bfd584b3f9ccd4de0d8e80184
Felipe Cantu <36748934+ABACABB00@users.noreply.github.com>
2020-06-23 00:09:37 -0500

    __dirname

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const api = require("./api");
 app.use("/api", api);
 
 if(process.env.PROD) {
-    app.use(express.static(path.join(_dirname, './client/build')));
+    app.use(express.static(path.join(__dirname, './client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, './client/build/index.html'));
     })
