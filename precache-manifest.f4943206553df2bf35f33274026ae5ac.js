df8e9c8628923389b274a4b7dacb22f10a42d2dd
Felipe Cantu Jr <cantujr.felipe@gmail.com>
2023-10-18 16:17:29 -0500

    Updates

---
+++ precache-manifest.f4943206553df2bf35f33274026ae5ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1852db183dfe4c22894947a74607d86d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "fe8b8cfec70e1726da31",
+    "url": "/clicky-game/static/css/main.296f786a.chunk.css"
+  },
+  {
+    "revision": "28e9acdd369bde8e9d62",
+    "url": "/clicky-game/static/js/2.d4b038ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.d4b038ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8b8cfec70e1726da31",
+    "url": "/clicky-game/static/js/main.7be26b04.chunk.js"
+  },
+  {
+    "revision": "d58cb5d367b3107da53c",
+    "url": "/clicky-game/static/js/runtime-main.38955922.js"
+  },
+  {
+    "revision": "ae7ca915749347a8aa7262353870bdfd",
+    "url": "/clicky-game/static/media/bg.ae7ca915.jpg"
+  }
+]);
